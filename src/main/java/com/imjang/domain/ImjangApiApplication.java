e70e42718ba0e8f834e4f54821c42b89987dbf5b
marista-dev <sa21788@naver.com>
2025-06-28 23:43:54 +0900

    chore: 프로젝트 구조 정리 및 설정 파일 재구성

--- src/main/java/com/imjang/domain/ImjangApiApplication.java
+++ src/main/java/com/imjang/domain/ImjangApiApplication.java
@@ -1,4 +1,4 @@
-package com.imjang;
+package com.imjang.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImjangApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImjangApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ImjangApiApplication.class, args);
+  }
 
 }
